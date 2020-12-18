ee9b3d4fcf1c95d563a4293b7487edba96261839
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-12-18 10:22:23 -0800

    rename list() to container()

--- js/models.js
+++ js/models.js
@@ -3,32 +3,21 @@ class TodoList {
     let whitelist = ["id", "name", "active"]
     whitelist.forEach(attr => this[attr] = attributes[attr])
   }
+
   /*
   TodoList.container() returns a reference to this DOM node:
-  <section id="todoListsContainer" class="px-4 bg-blue-100 min-h-screen rounded-md shadow">
-    <h1 class="text-2xl semibold border-b-4 border-blue">Todo Lists</h1>
-    <ul id="lists" class="list-none">
-
-    </ul>
-  </section>
-  */
-  static container() {
-    return this.c ||= document.querySelector("#todoListsContainer")
-  }
-  /*
-  TodoList.list() returns a reference to this DOM node:
   <ul id="lists" class="list-none">
 
   </ul>
   */
-  static list() {
-    return this.l ||= document.querySelector('#lists')
+  static container() {
+    return this.c ||= document.querySelector('#lists')
   }
 
   /*
-    TodoList.all() returns a promise for the collection of all todoList objects from the API.
-    It also takes those todoLists and calls render on them, generating the li DOM nodes that 
-    display them, and spreading them out into the list where they'll be appended to the DOM.
+  TodoList.all() returns a promise for the collection of all todoList objects from the API.
+  It also takes those todoLists and calls render on them, generating the li DOM nodes that 
+  display them, and spreading them out into the list where they'll be appended to the DOM.
   */
   static all() {
     return fetch("http://localhost:3000/todo_lists")
@@ -36,7 +25,7 @@ class TodoList {
       .then(todoListsJson => {
         this.collection = todoListsJson.map(tlAttributes => new TodoList(tlAttributes))
         let listItems = this.collection.map(list => list.render())
-        this.list().append(...listItems)
+        this.container().append(...listItems)
         return this.collection
       })
   }
@@ -64,7 +53,7 @@ class TodoList {
       .then(json => {
         let todoList = new TodoList(json);
         this.collection.push(todoList);
-        this.list().appendChild(todoList.render());
+        this.container().appendChild(todoList.render());
         new FlashMessage({type: 'success', message: 'TodoList added successfully'})
         return todoList;
       })
